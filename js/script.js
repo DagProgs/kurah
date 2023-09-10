9210086f1ca6156e99ba89ef7fa001e8f9fee3a6
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-09-10 18:54:50 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Получаем ссылку на элемент, в котором будут отображаться изображения и описания
+    const gallery = document.querySelector(".gallery");
+
+    // Загружаем JSON-файл с помощью fetch
+    fetch("https://dagprogs.github.io/apidb/kurakh/images.json")
+        .then(response => response.json())
+        .then(imageData => {
+            // Создаем элементы <img> и <p> для каждого изображения и его описания, и добавляем их в галерею
+            imageData.forEach(image => {
+                const description = document.createElement("p");
+                description.textContent = image.description;
+                gallery.appendChild(description);
+
+                const img = document.createElement("img");
+                img.src = image.src;
+                img.alt = image.alt;
+                gallery.appendChild(img);
+            });
+        })
+        .catch(error => {
+            console.error("Ошибка загрузки JSON:", error);
+        });
+});
