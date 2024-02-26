3c2eca47f7205afe59d52f75a4e72eb7d44d5c6c
DagProgs <114062397+DagProgs@users.noreply.github.com>
2024-02-26 21:25:03 +0300

    update

--- js/mounth.js
+++ js/mounth.js
@@ -21,32 +21,35 @@ function updatePrayerTimesTable() {
     var currentDate = new Date();
     var currentDay = currentDate.getDate();
 
+    // Clear table without losing the reference to existing rows
     tableBody.innerHTML = "";
 
     for (var day in prayerTimes[selectedMonth]) {
-        var row;
+        var row = document.createElement("tr");
 
         if (day == currentDay && selectedMonth == getCurrentMonth()) {
-            row = "<tr class='current-day-cell'>";
+            row.classList.add('current-day-cell');
             if (previousHighlightedDay) {
                 previousHighlightedDay.classList.remove('current-day-cell');
             }
-            previousHighlightedDay = tableBody.lastElementChild;
-        } else {
-            row = "<tr>";
+            previousHighlightedDay = row;
         }
 
-        row += "<td>" + day + "</td>";
+        var dayCell = document.createElement("td");
+        dayCell.textContent = day;
+        row.appendChild(dayCell);
 
         for (var prayer in prayerTimes[selectedMonth][day]) {
             var time = prayerTimes[selectedMonth][day][prayer];
             var formattedTime = time[0].toString().padStart(2, '0') + ":" + time[1].toString().padStart(2, '0');
-            row += "<td>" + formattedTime + "</td>";
+            var prayerCell = document.createElement("td");
+            prayerCell.textContent = formattedTime;
+            row.appendChild(prayerCell);
         }
 
-        row += "</tr>";
-        tableBody.innerHTML += row;
+        tableBody.appendChild(row);
     }
 }
+
 
 monthSelect.addEventListener("change", updatePrayerTimesTable);
