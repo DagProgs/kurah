d1c1ff04d192fe1e868aa97f71c43d3ec0cbb073
DagProgs <114062397+DagProgs@users.noreply.github.com>
2024-02-28 10:01:35 +0300

    up

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('push', function(event) {
+  const options = {
+    body: 'Время для намаза!',
+    icon: 'path/to/icon.png',
+    vibrate: [200, 100, 200, 100, 200, 100, 200],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification('Намаз', options)
+  );
+});
