0f8ac7c7fc9e877b328336aa087acfb9abf400fb
DagProgs <114062397+DagProgs@users.noreply.github.com>
2024-03-13 12:26:42 +0300

    update

--- sw-workbox.js
+++ sw-workbox.js
@@ -22,7 +22,7 @@ workbox.core.clientsClaim();
 workbox.precaching.precacheAndRoute([
   {
     "url": "index.html",
-    "revision": "8db2726819fcfd1ba0df7a22984483b6"
+    "revision": "8319aa7d426367e94abd3afba7bcfd4b"
   },
   {
     "url": "js/jquery-3.2.1.min.js",
