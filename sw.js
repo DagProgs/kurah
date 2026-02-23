5aa97eec7b00296f45f2bf5bb3a8f96f5ff399e1
DagProgs <114062397+DagProgs@users.noreply.github.com>
2026-02-23 11:21:06 +0300

    ddd

--- sw.js
+++ sw.js
@@ -1,20 +1,25 @@
 // public/sw.js
-self.addEventListener('install', () => {
-  console.log('Service Worker установлен');
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', () => {
-  console.log('Service Worker активирован');
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
-// Слушаем сообщения от основного приложения
+// Слушаем команду на запуск цикла
 self.addEventListener('message', (event) => {
-  if (event.data === 'start-timer') {
+  if (event.data === 'start-push-loop') {
+    // Внимание: setInterval в SW на мобилках живет недолго.
+    // Для реального фонового пуша нужен сервер. 
+    // Но для открытого/свернутого PWA этот метод сработает:
     setInterval(() => {
-      self.registration.showNotification('PWA Сообщение', {
-        body: 'Привет',
-        icon: '/vite.svg', // путь к иконке
+      self.registration.showNotification('PWA Уведомление', {
+        body: 'Привет!',
+        icon: '/vite.svg',
+        vibrate: [200, 100, 200],
+        tag: 'vibrate-sample'
       });
-    }, 10000); // 10 секунд
+    }, 10000);
   }
 });
